f73ed665f771060bc2f7b376f91745fff3910a68
Emma Troxell <troxellemma@gmail.com>
2020-01-13 14:27:43 -0600

    area studio

---
+++ studios/Area/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a radius:");
+                string input = Console.ReadLine();
+                try
+                {
+                    double radius = Convert.ToDouble(input);               
+                    double area = Math.PI * radius * radius;
+                    Console.WriteLine($"The area of a circle with a radius of {radius} is {area}");
+                } catch
+                {
+                    Console.WriteLine("Error: invalid user input");
+                }
+
+            }
+        }
+    }
+}
