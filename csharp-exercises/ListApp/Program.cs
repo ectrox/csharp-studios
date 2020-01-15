24df72dcc3feda1fd5aadfbc16124c7ae423b763
Emma Troxell <troxellemma@gmail.com>
2020-01-15 14:47:43 -0600

    corrected syntax error

---
+++ csharp-exercises/ListApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numList = new List<int>();
+
+            numList.Add(1);
+            numList.Add(2);
+            numList.Add(3);
+            numList.Add(4);
+            numList.Add(5);
+            numList.Add(6);
+            numList.Add(7);
+            numList.Add(8);
+            numList.Add(9);
+            numList.Add(10);
+
+            Console.WriteLine(ListSum(numList));
+            Console.ReadLine();
+        }
+
+        static int ListSum(List<int> list)
+        {
+            int sum = list.Sum();
+            return sum;
+        }
+    }
+}
